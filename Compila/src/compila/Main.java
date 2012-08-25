2d363ebbc794596b3bc33988764e8b86ba644b87
EDUARDO OLIVEIRA <emaildoedu00@gmail.com>
2012-08-25 18:35:37 -0300

    aaa

--- Compila/src/compila/Main.java
+++ Compila/src/compila/Main.java
@@ -1,8 +1,15 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ *
  */
 package compila;
+ 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.Console;
+
+
 
 /**
  *
@@ -15,6 +22,34 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic hereXZ
-        System.out.println("");
+        
+
+       Console console = System.console();
+        if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        while (true) {
+
+            Pattern pattern = 
+            Pattern.compile(console.readLine("%nEnter your regex: "));
+
+            Matcher matcher = 
+            pattern.matcher(console.readLine("Enter input string to search: "));
+
+            boolean found = false;
+            while (matcher.find()) {
+                console.format("I found the text" +
+                    " \"%s\" starting at " +
+                    "index %d and ending at index %d.%n",
+                    matcher.group(),
+                    matcher.start(),
+                    matcher.end());
+                found = true;
+            }
+            if(!found){
+                console.format("No match found.%n");
+            }
+        }
     }
 }
