56e3fa294d7abd1fda1d3d11bb5801e00b14a90b
EDUARDO OLIVEIRA <emaildoedu00@gmail.com>
2012-08-25 18:20:32 -0300

    aaa

--- Compila/src/compila/Compila.java
+++ Compila/src/compila/Compila.java
@@ -14,6 +14,7 @@ public class Compila {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic hereXZ
+        System.out.println("");
     }
 }
