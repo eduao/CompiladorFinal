9c9e5d81dec9658d0f15874b2bc60a6961ec3392
Augusto Ferreira Andrade do Carmo <auuuguuusto@yahoo.com.br>
2012-09-09 19:58:07 -0300

    compila - modificacao na modificacao da modificacao do Edu

--- Compila/src/compila/AnalisadorSintatico.java
+++ Compila/src/compila/AnalisadorSintatico.java
@@ -9,11 +9,15 @@ package compila;
  * @author Augusto FADC
  */
 public class AnalisadorSintatico {
-
+    
     public void casaToken(String token) {
     }
+    
+    public void erroSintatico() {
+        System.out.println("ERRO SINTATICO =/");
+    }
     String token;
-    String ID, CONST;//AINDA NAO SEI O QUE FAZER
+    String ID, CONST, QUEBRA_DE_LINHA;//AINDA NAO SEI O QUE FAZER
 
     public void INICIO() {
         while (!(token.equals("DPonto"))
@@ -26,7 +30,7 @@ public class AnalisadorSintatico {
             DECLARACOES_VARIAVEIS_INICIAIS();
         }
         casaToken("Inicio");
-        casaToken("<R>");
+        casaToken("QUEBRA");
         while (!(token.equals("Luz"))
                 || !(token.equals("Escala"))
                 || !(token.equals(ID))
@@ -36,16 +40,16 @@ public class AnalisadorSintatico {
                 || !(token.equals("pause"))) {
             CODIGO();
         }
-        casaToken("<R>");
+        casaToken(QUEBRA_DE_LINHA);
         casaToken("Fim");
-
-
+        
+        
     }
-
+    
     public void DECLARACOES_VARIAVEIS_INICIAIS() {
         if (token.equals("DPonto")) {
             casaToken("DPonto");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -53,7 +57,7 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DCor")) {
             casaToken("DCor");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -61,7 +65,7 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DFace")) {
             casaToken("DFace");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -69,7 +73,7 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DLuz")) {
             casaToken("DLuz");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -77,7 +81,7 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DObjeto")) {
             casaToken("DObjeto");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -85,7 +89,7 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DConst")) {
             casaToken("DConst");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
@@ -93,32 +97,253 @@ public class AnalisadorSintatico {
             }
         } else if (token.equals("DVar")) {
             casaToken("DVar");
-            casaToken("<R>");
+            casaToken(QUEBRA_DE_LINHA);
             while (token.equals(ID)) {
                 casaToken(ID);
                 casaToken("=");
                 DVAR();
             }
         }
-
-
+        
+        
     }
-
+    
     public void REPETICAO() {
+        casaToken("enquanto");
+        if (token.equals(CONST)) {
+            casaToken(CONST);
+        } else if (token.equals(ID)) {
+            casaToken(ID);
+        } else {
+            erroSintatico();
+        }
+        OPERADOR_CONDICIONAL();
+        if (token.equals(CONST)) {
+            casaToken(CONST);
+        } else if (token.equals(ID)) {
+            casaToken(ID);
+        } else {
+            erroSintatico();
+        }
+        casaToken("faca");
+        if (token.equals(QUEBRA_DE_LINHA)) {
+            casaToken(QUEBRA_DE_LINHA);
+            casaToken("inicio");
+            casaToken(QUEBRA_DE_LINHA);
+            while (!(token.equals("Luz"))
+                    || !(token.equals("Escala"))
+                    || !(token.equals(ID))
+                    || !(token.equals("Rottrans"))
+                    || !(token.equals("enquanto"))
+                    || !(token.equals("se"))
+                    || !(token.equals("pause"))) {
+                CODIGO();
+            }
+            casaToken("fim");
+            casaToken(QUEBRA_DE_LINHA);
+            
+        } else if ((token.equals("Luz"))
+                || (token.equals("Escala"))
+                || (token.equals(ID))
+                || (token.equals("Rottrans"))
+                || (token.equals("enquanto"))
+                || (token.equals("se"))
+                || (token.equals("pause"))) {
+            CODIGO();
+        } else {
+            erroSintatico();
+        }
+        
     }
-
+    
     public void CONDICIONAL_SE() {
+        casaToken("se");
+        if (token.equals(CONST)) {
+            casaToken(CONST);
+        } else if (token.equals(ID)) {
+            casaToken(ID);
+        } else {
+            erroSintatico();
+        }
+        OPERADOR_CONDICIONAL();
+        if (token.equals(CONST)) {
+            casaToken(CONST);
+        } else if (token.equals(ID)) {
+            casaToken(ID);
+        } else {
+            erroSintatico();
+        }
+        casaToken("entao");
+        if (token.equals(QUEBRA_DE_LINHA)) {
+            casaToken(QUEBRA_DE_LINHA);
+            casaToken("inicio");
+            casaToken(QUEBRA_DE_LINHA);
+            while (!(token.equals("Luz"))
+                    || !(token.equals("Escala"))
+                    || !(token.equals(ID))
+                    || !(token.equals("Rottrans"))
+                    || !(token.equals("enquanto"))
+                    || !(token.equals("se"))
+                    || !(token.equals("pause"))) {
+                CODIGO();
+            }
+            casaToken("fim");
+            casaToken(QUEBRA_DE_LINHA);
+            
+        } else if ((token.equals("Luz"))
+                || (token.equals("Escala"))
+                || (token.equals(ID))
+                || (token.equals("Rottrans"))
+                || (token.equals("enquanto"))
+                || (token.equals("se"))
+                || (token.equals("pause"))) {
+            CODIGO();
+        } else {
+            erroSintatico();
+        }
+        if (token.equals("senao")) {
+            CONDICIONAL_SENAO();
+        }
     }
-
+    
     public void CONDICIONAL_SENAO() {
+        casaToken("senao");
+        if (token.equals(QUEBRA_DE_LINHA)) {
+            casaToken(QUEBRA_DE_LINHA);
+            casaToken("inicio");
+            casaToken(QUEBRA_DE_LINHA);
+            while (!(token.equals("Luz"))
+                    || !(token.equals("Escala"))
+                    || !(token.equals(ID))
+                    || !(token.equals("Rottrans"))
+                    || !(token.equals("enquanto"))
+                    || !(token.equals("se"))
+                    || !(token.equals("pause"))) {
+                CODIGO();
+            }
+            casaToken("fim");
+            casaToken(QUEBRA_DE_LINHA);
+            
+        } else if ((token.equals("Luz"))
+                || (token.equals("Escala"))
+                || (token.equals(ID))
+                || (token.equals("Rottrans"))
+                || (token.equals("enquanto"))
+                || (token.equals("se"))
+                || (token.equals("pause"))) {
+            CODIGO();
+        } else {
+            erroSintatico();
+        }
     }
-
+    
     public void OPERADOR_CONDICIONAL() {
+        if (token.equals(">")) {
+            
+        } else if (token.equals(">=")) {
+            
+        } else if (token.equals("<")) {
+            
+        } else if (token.equals("<=")) {
+            
+        } else if (token.equals("==")) {
+            
+        } else if (token.equals("<>")) {
+            
+        } else {
+            erroSintatico();
+        }
     }
-
+    
     public void CODIGO() {
+        if (token.equals("Luz")) {
+            casaToken("Luz");
+            casaToken(ID);
+        } else if (token.equals("Escala")) {
+            casaToken("Escala");
+            casaToken(ID);
+            casaToken(",");
+            casaToken(ID);
+        } else if (token.equals(ID)) {
+            casaToken(ID);
+            casaToken("=");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+        } else if (token.equals("Rottrans")) {
+            casaToken("Rottrans");
+            casaToken(ID);
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+            casaToken(",");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+        } else if (token.equals("enquanto")) {//repeticao
+            REPETICAO();
+        } else if (token.equals("se")) {//condicional se
+            CONDICIONAL_SE();
+        } else if (token.equals("pausa")) {
+            casaToken("pausa");
+            if (token.equals(CONST)) {
+                casaToken(CONST);
+            }else if(token.equals(ID)){
+                casaToken(ID);
+            }else{
+                erroSintatico();
+            }
+        } else {
+            erroSintatico();
+        }
+        casaToken(QUEBRA_DE_LINHA);
     }
-
+    
     public void DPONTO() {
         casaToken("(");
         if (token.equals("-")) {
@@ -137,53 +362,67 @@ public class AnalisadorSintatico {
         casaToken(CONST);
         casaToken(")");
     }
-
+    
     public void DCOR() {
         casaToken("(");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(",");
-        if(token.equals(CONST)){
+        if (token.equals(CONST)) {
             casaToken(CONST);
-        }else if (token.equals(ID)){
+        } else if (token.equals(ID)) {
             casaToken(ID);
+        } else {
+            erroSintatico();
         }
         casaToken(")");
     }
-
+    
     public void DFACE() {
         casaToken("(");
         casaToken(ID);
@@ -197,7 +436,7 @@ public class AnalisadorSintatico {
         casaToken(ID);
         casaToken(")");
     }
-
+    
     public void DLUZ() {
         casaToken("(");
         if (token.equals("-")) {
@@ -216,7 +455,7 @@ public class AnalisadorSintatico {
         casaToken(CONST);
         casaToken(")");
     }
-
+    
     public void DOBJETO() {
         casaToken(ID);
         casaToken(",");
@@ -230,17 +469,69 @@ public class AnalisadorSintatico {
         casaToken(",");
         casaToken(ID);
     }
-
+    
     public void DCONST() {
-        
+        casaToken(ID);
+        casaToken("=");
+        if (token.equals("-")) {
+            casaToken("-");
+        }
+        casaToken(CONST);//numerica -> ver o que fazer -> verificar se há apenas numeros?
     }
-
+    
     public void DVAR() {
+        if (token.equals("inteiro")) {
+            casaToken("inteiro");
+        } else if (token.equals("real")) {
+            casaToken("real");
+        }
+        casaToken(CONST);
+        while ((token.equals("-"))
+                || (token.equals(CONST))) {
+            if (token.equals("-")) {
+                casaToken("-");
+            }
+            casaToken(CONST);
+        }
     }
-
+    
     public void EXPRESSAO_MATEMATICA() {
+        if (token.equals("-")) {
+            casaToken("-");
+        }
+        if (token.equals(ID)) {
+            casaToken(ID);
+        } else if (token.equals(CONST)) {
+            casaToken(CONST);
+        } else {
+            erroSintatico();
+        }
+        while (!(token.equals("+")
+                || token.equals("-")
+                || token.equals("*")
+                || token.equals("/"))) {
+            OPERADOR_MATEMATICO();
+            if (token.equals(ID)) {
+                casaToken(ID);
+            } else if (token.equals(CONST)) {
+                casaToken(CONST);
+            } else {
+                erroSintatico();
+            }
+        }
     }
-
+    
     public void OPERADOR_MATEMATICO() {
+        if (token.equals("+")) {
+            casaToken("+");
+        } else if (token.equals("-")) {
+            casaToken("-");
+        } else if (token.equals("*")) {
+            casaToken("*");
+        } else if (token.equals("/")) {
+            casaToken("/");
+        } else {
+            erroSintatico();
+        }
     }
 }
