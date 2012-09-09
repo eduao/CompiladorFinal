09e9f6433dc7e4e2ca8ba7618aef932b97e67e3b
Augusto Ferreira Andrade do Carmo <auuuguuusto@yahoo.com.br>
2012-09-08 22:16:51 -0300

    compila - modificacao na modificacao da modificacao do Edu

---
+++ Compila/src/compila/AnalisadorSintatico.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compila;
+
+/**
+ *
+ * @author Augusto FADC
+ */
+public class AnalisadorSintatico {
+
+    public void INICIO() {
+    }
+
+    public void DECLARACOES_VARIAVEIS_INICIAIS() {
+    }
+
+    public void REPETICAO() {
+    }
+
+    public void CONDICIONAL_SE() {
+    }
+
+    public void CONDICIONAL_SENAO() {
+    }
+
+    public void OPERADOR_CONDICIONAL() {
+    }
+
+    public void CODIGO() {
+    }
+
+    public void DPONTO() {
+    }
+
+    public void DCOR() {
+    }
+
+    public void DFACE() {
+    }
+
+    public void DLUZ() {
+    }
+
+    public void DOBJETO() {
+    }
+
+    public void DCONST() {
+    }
+
+    public void DVAR() {
+    }
+
+    public void EXPRESSAO_MATEMATICA() {
+    }
+
+    public void OPERADOR_MATEMATICO() {
+    }
+}
